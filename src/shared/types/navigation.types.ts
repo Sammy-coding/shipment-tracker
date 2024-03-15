ff988b0057cc060b19c1075e9238dc915c7b71b9
sammy-coding <samuelodebunmi1@gmail.com>
2024-03-15 21:53:29 +0100

    added a navigation ts file

---
+++ src/shared/types/navigation.types.ts
@@ -0,0 +1,11 @@
+export type AuthParamList = {
+  LOGIN_SCREEN: undefined;
+};
+
+export type AppParamList = {
+  SHIPPMENT_SCREEN: undefined;
+  SCAN_SCREEN: undefined;
+  ALLET_SCREEN: undefined;
+  PROFILE_SCREEN: undefined;
+  SPLASH_SCREEN: undefined;
+};
