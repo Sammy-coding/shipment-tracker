6f16733b182bf90aae417405db92bb33d3b35257
sammy-coding <samuelodebunmi1@gmail.com>
2024-03-15 21:53:15 +0100

    added responsive func

---
+++ src/shared/utils/responsive/index.ts
@@ -0,0 +1,12 @@
+import {
+  widthPercentageToDP as wp2dp,
+  heightPercentageToDP as hp2dp,
+} from 'react-native-responsive-screen';
+
+export const wp = (dimension: number) => {
+  return wp2dp((dimension / 428) * 100 + '%');
+};
+
+export const hp = (dimension: number) => {
+  return hp2dp((dimension / 926) * 100 + '%');
+};
